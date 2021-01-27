03366b3aad47519fc462db8c5782eca1f02999cb
whitekiwi <jh145478@gmail.com>
2021-01-27 21:22:33 +0900

    test(): Set global setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	globalSetup: '<rootDir>/../test/global-setup/setup.ts',
+	moduleFileExtensions: ['js', 'json', 'ts'],
+	rootDir: 'src',
+	testRegex: '.*\\.spec\\.ts$',
+	transform: {
+		'^.+\\.(t|j)s$': 'ts-jest',
+	},
+	collectCoverageFrom: ['**/*.(t|j)s'],
+	coverageDirectory: '../coverage',
+	testEnvironment: 'node',
+}
