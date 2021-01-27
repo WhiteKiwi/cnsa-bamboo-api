549912a5f174add3e496533bbbfdaed9a67cd50e
whitekiwi <jh145478@gmail.com>
2021-01-27 17:06:14 +0900

    test(): Add typeorm module to e2e test

--- test/e2e/app.e2e-spec.ts
+++ test/e2e/app.e2e-spec.ts
@@ -6,13 +6,34 @@ import { AppService } from '../../src/app.service'
 import { AppController } from '../../src/app.controller'
 
 import GetConfigModule from '../../src/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 describe('AppController (e2e)', () => {
 	let app: INestApplication
 
 	beforeEach(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
-			imports: [GetConfigModule({ isTest: true })],
+			imports: [
+				GetConfigModule({ isTest: true }),
+				TypeOrmModule.forRootAsync({
+					imports: [ConfigModule],
+					useFactory: (configService: ConfigService) => ({
+						type: 'mysql',
+						host: configService.get('TYPEORM_HOST'),
+						port: +configService.get<number>('TYPEORM_PORT'),
+						database: configService.get('TYPEORM_DATABASE'),
+						username: configService.get('TYPEORM_USERNAME'),
+						password: configService.get('TYPEORM_PASSWORD'),
+
+						entities: [configService.get('TYPEORM_ENTITIES')],
+						migrations: [configService.get('TYPEORM_MIGRATIONS')],
+						// This value must be false! - https://typeorm.io/#/connection-options/common-connection-options
+						synchronize: false,
+					}),
+					inject: [ConfigService],
+				}),
+			],
 			controllers: [AppController],
 			providers: [AppService],
 		}).compile()
