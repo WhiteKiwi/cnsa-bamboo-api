6ea88db26172b891917cfb5971933f9fe3729fb9
whitekiwi <jh145478@gmail.com>
2021-01-31 18:17:52 +0900

    fix(): Fix param typo in questions controller

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Post } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 
 import { QuestionsService } from './questions.service'
 
@@ -7,7 +7,7 @@ export class QuestionsController {
 	constructor(private readonly reportsService: QuestionsService) {}
 
 	@Post()
-	async create(@Param('question') question: string) {
+	async create(@Body('question') question: string) {
 		await this.reportsService.create({ content: question })
 	}
 }
