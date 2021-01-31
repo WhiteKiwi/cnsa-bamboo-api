c1820fa3013c02539f2e7f5ab911d70ed61bd1d3
whitekiwi <jh145478@gmail.com>
2021-01-31 19:54:30 +0900

    test(): Add service test to ReportsService.find

--- src/reports/reports.service.spec.ts
+++ src/reports/reports.service.spec.ts
@@ -3,10 +3,13 @@ import { ReportsService } from './reports.service'
 import { getConfigModule, getTypeOrmModule } from '../modules'
 
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { getRepository, Repository } from 'typeorm'
 import { Report } from '../entities'
+import { REPORT_STATUS } from '../utils'
 
 describe('ReportsService', () => {
 	let service: ReportsService
+	let questionRepository: Repository<Report>
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -19,9 +22,36 @@ describe('ReportsService', () => {
 		}).compile()
 
 		service = module.get<ReportsService>(ReportsService)
+		questionRepository = getRepository(Report)
 	})
 
-	it('should be defined', () => {
-		expect(service).toBeDefined()
+	it('Should be get all reports', async () => {
+		const reports = await service.find()
+		const reportsInDb = await questionRepository.find()
+
+		expect(reports).toEqual(reportsInDb)
+	})
+
+	it('Reports should be filterd by status', async () => {
+		const reportedReports = await service.find({
+			status: REPORT_STATUS.REPORTED,
+		})
+		for (const report of reportedReports) {
+			expect(report.status).toBe(REPORT_STATUS.REPORTED)
+		}
+
+		const appliedReports = await service.find({
+			status: REPORT_STATUS.APPLIED,
+		})
+		for (const report of appliedReports) {
+			expect(report.status).toBe(REPORT_STATUS.APPLIED)
+		}
+
+		const deniedReports = await service.find({
+			status: REPORT_STATUS.DENIED,
+		})
+		for (const report of deniedReports) {
+			expect(report.status).toBe(REPORT_STATUS.DENIED)
+		}
 	})
 })
