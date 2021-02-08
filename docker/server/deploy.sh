b32a9d111fad4dbdfc3b05eaf58e28245060aae2
whitekiwi <jh145478@gmail.com>
2021-02-08 10:33:15 +0900

    cd(): Fix typo

--- docker/server/deploy.sh
+++ docker/server/deploy.sh
@@ -1,3 +1,4 @@
 docker login --username AWS -p $(aws ecr get-login-password --region ap-northeast-2 --profile kiwi) 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com
-docker build -t 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:$1 .
+docker build --no-cache -t 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:$1 .
+docker tag 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:$1 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:latest
 docker push 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:$1
