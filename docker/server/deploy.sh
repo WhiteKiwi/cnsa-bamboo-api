5272fc8172bd229aee41e173057c699801dfeb77
whitekiwi <jh145478@gmail.com>
2021-02-09 16:57:27 +0900

    cd(): Remove images after deploy

--- docker/server/deploy.sh
+++ docker/server/deploy.sh
@@ -2,3 +2,6 @@ docker login --username AWS -p $(aws ecr get-login-password --region ap-northeas
 docker build --no-cache -t 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:$1 . --build-arg BRANCH=$(git branch --show-current)
 docker tag 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:$1 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:latest
 docker push 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:$1
+docker push 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:latest
+docker rmi 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:$1
+docker rmi 138497848618.dkr.ecr.ap-northeast-2.amazonaws.com/cnsa-bamboo-api:latest
