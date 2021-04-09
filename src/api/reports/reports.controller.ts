c3364fec17ef7421b3c351eb9f50b4a148e062a8
whitekiwi <jh145478@gmail.com>
2021-04-09 15:20:53 +0900

    feat(): Add swagger

--- src/api/reports/reports.controller.ts
+++ src/api/reports/reports.controller.ts
@@ -6,10 +6,18 @@ import { Report } from '../../typeorm/entities'
 import { REPORT_STATUS } from '../../utils/types'
 import { UpperCasePipe } from '../../utils/pipes'
 
+import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger'
+
+@ApiTags('Report')
 @Controller('reports')
 export class ReportsController {
 	constructor(private readonly reportsService: ReportsService) {}
 
+	@ApiOperation({
+		summary: '제보들을 조회합니다',
+	})
+	@ApiQuery({ name: 'status', enum: REPORT_STATUS, required: false })
+	// TODO: @ApiResponse
 	@Get()
 	async find(
 		@Query('status', UpperCasePipe)
