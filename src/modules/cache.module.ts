49811957a235b552d958c0d259d1b176cf3ea10a
whitekiwi <jh145478@gmail.com>
2021-05-09 16:35:22 +0900

    feat(Cache): Add redis cache type

--- src/modules/cache.module.ts
+++ src/modules/cache.module.ts
@@ -1,5 +1,27 @@
 import { CacheModule } from '@nestjs/common'
+import * as redisStore from 'cache-manager-redis-store'
 
-export function getCacheModule({ ttl = 5 } = { ttl: 5 }) {
+export type CacheType = 'mem-cache' | 'redis'
+type GetCacheModuleParameters = {
+	type: CacheType
+	ttl: number
+}
+const DEFAULT_GET_CACHE_MODULE_PARAMETERS: GetCacheModuleParameters = {
+	type: 'mem-cache',
+	ttl: 5,
+}
+export function getCacheModule({
+	type = DEFAULT_GET_CACHE_MODULE_PARAMETERS.type,
+	ttl = DEFAULT_GET_CACHE_MODULE_PARAMETERS.ttl,
+}: GetCacheModuleParameters = DEFAULT_GET_CACHE_MODULE_PARAMETERS) {
+	if (type === 'redis') {
+		return CacheModule.register({
+			ttl,
+			store: redisStore,
+			host: 'localhost',
+			port: 6379,
+		})
+	}
+
 	return CacheModule.register({ ttl })
 }
