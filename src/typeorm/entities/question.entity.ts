0b6079128992e46a381881b084d0de26890819a6
whitekiwi <jh145478@gmail.com>
2021-05-09 16:10:58 +0900

    migration(Answer): Remove entity, create json field into question entity

--- src/typeorm/entities/question.entity.ts
+++ src/typeorm/entities/question.entity.ts
@@ -1,12 +1,11 @@
-import { Entity, Column, OneToMany } from 'typeorm'
+import { Entity, Column } from 'typeorm'
 import { PrimaryIdWithDateColumns } from '../entity-bases'
-import { Answer } from '.'
 
 @Entity({ name: 'questions' })
 export class Question extends PrimaryIdWithDateColumns {
 	@Column('varchar', { length: 500, unique: true })
 	content: string
 
-	@OneToMany(() => Answer, (answer) => answer.question)
-	answers: Answer[]
+	@Column('json', { nullable: false, default: [] })
+	answers: string[]
 }
