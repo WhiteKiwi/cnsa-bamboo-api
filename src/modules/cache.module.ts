7b6bdd0a214455ac96920955a444c884e7b21053
whitekiwi <jh145478@gmail.com>
2021-05-09 17:55:32 +0900

    fix(ConfigModule): Fix redis cache module options

--- src/modules/cache.module.ts
+++ src/modules/cache.module.ts
@@ -1,10 +1,12 @@
 import { CacheModule } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import * as redisStore from 'cache-manager-redis-store'
+import { REDIS } from '../config'
 
 export type CacheType = 'mem-cache' | 'redis'
 type GetCacheModuleParameters = {
-	type: CacheType
-	ttl: number
+	type?: CacheType
+	ttl?: number
 }
 const DEFAULT_GET_CACHE_MODULE_PARAMETERS: GetCacheModuleParameters = {
 	type: 'mem-cache',
@@ -15,11 +17,17 @@ export function getCacheModule({
 	ttl = DEFAULT_GET_CACHE_MODULE_PARAMETERS.ttl,
 }: GetCacheModuleParameters = DEFAULT_GET_CACHE_MODULE_PARAMETERS) {
 	if (type === 'redis') {
-		return CacheModule.register({
-			ttl,
-			store: redisStore,
-			host: 'localhost',
-			port: 6379,
+		return CacheModule.registerAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				ttl,
+				store: redisStore,
+				host: configService.get(REDIS.HOST),
+				port: configService.get(REDIS.PORT),
+				auth_user: configService.get(REDIS.USER),
+				auth_pass: configService.get(REDIS.PASSWORD),
+			}),
+			inject: [ConfigService],
 		})
 	}
 
